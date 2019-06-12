3b9549fe5f37eef235e98a953dc3f1b4673df9ec
Matthew Serre <matthew.serre@gmail.com>
2019-06-12 21:29:42 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -13,6 +13,8 @@ class App < Sinatra::Base
   post '/puppy' do
     @puppy = Puppy.new(params[:name],params[:breed],params[:age])
     @name = @puppy.name
+    @breed = @puppy.breed
+    @age = @puppy.months_old
     
     erb :display_puppy
   end
